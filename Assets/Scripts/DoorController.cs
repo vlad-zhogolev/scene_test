44ea5a36842dbffc66baa5e7828b5980256545d1
zvladyuha <z.vladyuha@yandex.ru>
2020-02-09 22:50:09 +0300

    1

---
+++ Assets/Scripts/DoorController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorController : MonoBehaviour
+{
+    Animator animator;
+
+    [SerializeField]
+    bool changeState = false;
+
+    GameObject doorHand;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if (changeState)
+        {
+            changeState = false;
+            animator.SetBool("isOpen", !animator.GetBool("isOpen"));
+        }
+    }
+}
---
+++ Assets/Scripts/SwitchController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchController : MonoBehaviour
+{
+    [SerializeField]
+    private Light[] lights;
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.transform.root.gameObject.tag == NetworkingPlayer.PLAYER_TAG &&
+            OVRInput.GetUp(OVRInput.Button.One))
+        {
+            foreach (var light in lights)
+            {
+                light.enabled = !light.enabled;
+            }
+        }
+    }
+}
