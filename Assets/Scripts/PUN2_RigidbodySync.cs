43601758d64da9a8521450e277b9d15dd6b91334
zvladyuha <z.vladyuha@yandex.ru>
2020-02-10 23:41:37 +0300

    2

--- Assets/Scripts/PUN2_RigidbodySync.cs
+++ Assets/Scripts/PUN2_RigidbodySync.cs
@@ -18,16 +18,16 @@ public class PUN2_RigidbodySync : MonoBehaviourPun, IPunObservable
     void Start()
     {
         r = GetComponent<Rigidbody>();
-        Debug.LogFormat("PUN2_RigidbodySync: received rigidbody = {0}", r != null);
-        Debug.LogFormat("PUN2_RigidbodySync: start IsMine = {0}", photonView.IsMine);
+        //Debug.LogFormat("PUN2_RigidbodySync: received rigidbody = {0}", r != null);
+        //Debug.LogFormat("PUN2_RigidbodySync: start IsMine = {0}", photonView.IsMine);
     }
 
     public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
     {
-        Debug.Log("PUN2_RigidbodySync: OnPhotonSerializeView");
+        //Debug.Log("PUN2_RigidbodySync: OnPhotonSerializeView");
         if (stream.IsWriting)
         {
-            Debug.LogFormat("PUN2_RigidbodySync: Writing position for {0}", gameObject.name);
+            //Debug.LogFormat("PUN2_RigidbodySync: Writing position for {0}", gameObject.name);
             //We own this player: send the others our data
             stream.SendNext(transform.position);
             stream.SendNext(transform.rotation);
@@ -36,7 +36,7 @@ public class PUN2_RigidbodySync : MonoBehaviourPun, IPunObservable
         }
         else
         {
-            Debug.LogFormat("PUN2_RigidbodySync: Reading position for {0}", gameObject.name);
+            //Debug.LogFormat("PUN2_RigidbodySync: Reading position for {0}", gameObject.name);
             //Network player, receive data
             latestPos = (Vector3)stream.ReceiveNext();
             latestRot = (Quaternion)stream.ReceiveNext();
@@ -52,12 +52,12 @@ public class PUN2_RigidbodySync : MonoBehaviourPun, IPunObservable
     {
         if (photonViewOwnerChanged)
         {
-            Debug.LogFormat("PUN2_RigidbodySync: photonView of object {0} changed, IsMine = {1}", gameObject.name, photonView.IsMine);
+            //Debug.LogFormat("PUN2_RigidbodySync: photonView of object {0} changed, IsMine = {1}", gameObject.name, photonView.IsMine);
             photonViewOwnerChanged = false;
         }
         if (!photonView.IsMine && valuesReceived)
         {
-            Debug.LogFormat("PUN2_RigidbodySync: Update rigidbody: {0}", gameObject.name);
+            //Debug.LogFormat("PUN2_RigidbodySync: Update rigidbody: {0}", gameObject.name);
             //Update Object position and Rigidbody parameters
             transform.position = Vector3.Lerp(transform.position, latestPos, Time.deltaTime * 5);
             transform.rotation = Quaternion.Lerp(transform.rotation, latestRot, Time.deltaTime * 5);
@@ -68,15 +68,15 @@ public class PUN2_RigidbodySync : MonoBehaviourPun, IPunObservable
 
     void OnCollisionEnter(Collision contact)
     {
-        Debug.LogFormat("PUN2_RigidbodySync: Collision detected, this: {0}, other: {1}, photonView.IsMine: {2}", gameObject.name, contact.gameObject.name, photonView.IsMine);
+        //Debug.LogFormat("PUN2_RigidbodySync: Collision detected, this: {0}, other: {1}, photonView.IsMine: {2}", gameObject.name, contact.gameObject.name, photonView.IsMine);
         if (!photonView.IsMine)
         {
             Transform collisionObjectRoot = contact.transform.root;
-            Debug.LogFormat("PUN2_RigidbodySync: Collision collisionObjectRoot: {0}, it's tag: {1}", collisionObjectRoot.gameObject.name, collisionObjectRoot.tag);
+            //Debug.LogFormat("PUN2_RigidbodySync: Collision collisionObjectRoot: {0}, it's tag: {1}", collisionObjectRoot.gameObject.name, collisionObjectRoot.tag);
             if (collisionObjectRoot.CompareTag(NetworkingPlayer.PLAYER_TAG))
             {
                 //Transfer PhotonView of Rigidbody to our local player
-                Debug.LogFormat("PUN2_RigidbodySync: Transferring ownership to: {0}", PhotonNetwork.LocalPlayer);
+                //Debug.LogFormat("PUN2_RigidbodySync: Transferring ownership to: {0}", PhotonNetwork.LocalPlayer);
                 photonView.TransferOwnership(PhotonNetwork.LocalPlayer);
                 photonViewOwnerChanged = true;
             }
@@ -85,15 +85,15 @@ public class PUN2_RigidbodySync : MonoBehaviourPun, IPunObservable
 
     private void OnTriggerEnter(Collider other)
     {
-        Debug.LogFormat("PUN2_RigidbodySync: Trigger enter detected, this: {0}, other: {1}, photonView.IsMine: {2}", gameObject.name, other.gameObject.name, photonView.IsMine);
+        //Debug.LogFormat("PUN2_RigidbodySync: Trigger enter detected, this: {0}, other: {1}, photonView.IsMine: {2}", gameObject.name, other.gameObject.name, photonView.IsMine);
         if (!photonView.IsMine)
         {
             Transform collisionObjectRoot = other.transform.root;
-            Debug.LogFormat("PUN2_RigidbodySync: Trigger collisionObjectRoot: {0}, it's tag: {1}", collisionObjectRoot.gameObject.name, collisionObjectRoot.tag);
+            //Debug.LogFormat("PUN2_RigidbodySync: Trigger collisionObjectRoot: {0}, it's tag: {1}", collisionObjectRoot.gameObject.name, collisionObjectRoot.tag);
             if (collisionObjectRoot.CompareTag(NetworkingPlayer.PLAYER_TAG))
             {
                 //Transfer PhotonView of Rigidbody to our local player
-                Debug.LogFormat("PUN2_RigidbodySync: Transferring ownership to: {0}", PhotonNetwork.LocalPlayer);
+                //Debug.LogFormat("PUN2_RigidbodySync: Transferring ownership to: {0}", PhotonNetwork.LocalPlayer);
                 photonView.TransferOwnership(PhotonNetwork.LocalPlayer);
                 photonViewOwnerChanged = true;
             }
--- Assets/Scripts/SwitchController.cs
+++ Assets/Scripts/SwitchController.cs
@@ -1,21 +1,64 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using Photon.Pun;
 
-public class SwitchController : MonoBehaviour
+public class SwitchController : MonoBehaviour, IPunObservable
 {
     [SerializeField]
     private Light[] lights;
 
-    private void OnTriggerStay(Collider other)
+    [SerializeField]
+    private bool isTurnedOn;
+
+    bool isPlayerInteracts = false;
+
+    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
     {
-        if (other.transform.root.gameObject.tag == NetworkingPlayer.PLAYER_TAG &&
-            OVRInput.GetUp(OVRInput.Button.One))
+        if (stream.IsWriting)
         {
+            Debug.Log("SwitchController: send light state");
+            stream.SendNext(isTurnedOn);
+        }
+        else
+        {
+            Debug.Log("SwitchController: received light state");
+            isTurnedOn = (bool)stream.ReceiveNext();
             foreach (var light in lights)
             {
-                light.enabled = !light.enabled;
+                light.enabled = isTurnedOn;
             }
+        }
+    }
+
+    void Update()
+    {
+        bool isRIndexTriggerPressed = OVRInput.Get(OVRInput.RawButton.RIndexTrigger);
+        bool isButtonReleased = OVRInput.GetUp(OVRInput.Button.One);
+        if (isPlayerInteracts && (isRIndexTriggerPressed || isButtonReleased))
+        {
+            Debug.LogFormat("SwitchController: Switch lights, isRIndexTriggerPressed = {0}, isButtonReleased = {1}", isRIndexTriggerPressed, isButtonReleased);
+            isTurnedOn = !isTurnedOn;
+            foreach (var light in lights)
+            {
+                light.enabled = isTurnedOn;
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        isPlayerInteracts = (other.transform.root.gameObject.CompareTag(NetworkingPlayer.PLAYER_TAG));
+        if (isPlayerInteracts)
+        {
+            Debug.Log("SwitchController: Player interacts with switch");
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        isPlayerInteracts = !(other.transform.root.gameObject.CompareTag(NetworkingPlayer.PLAYER_TAG));
+        if (!isPlayerInteracts)
+        {
+            Debug.Log("SwitchController: Player ended interaction with switch");
         }
     }
 }
